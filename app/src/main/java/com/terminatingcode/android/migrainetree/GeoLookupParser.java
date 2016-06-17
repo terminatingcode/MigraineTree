809b5929fce25164a4026ea32e3f274e7a9e3cd8
cannedfruit <sarah.a.connor@gmail.com>
2016-06-16 21:11:33 -0400

    GeoLookupParser now passes all tests

---
+++ app/src/main/java/com/terminatingcode/android/migrainetree/GeoLookupParser.java
@@ -0,0 +1,60 @@
+package com.terminatingcode.android.migrainetree;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Parses the json returned when looking up a location
+ * results may be multiple cities, just one city, or none
+ * Created by Sarah on 6/16/2016.
+ */
+public class GeoLookupParser {
+
+    public String[] parse(JSONObject json) throws JSONException {
+        if(json == null) return new String[]{"null JSONObject"};
+        if(json.has("results")) {
+            return extractResults(json.getJSONArray("results"));
+        }else if(json.has("error")){
+            return new String[]{"city not found"};
+        }else if(json.has("location")){
+            String[] location = new String[1];
+            location[0] = parseCity(json.getJSONObject("location"));
+            return location;
+        }
+        else return new String[]{"huh"};
+    }
+
+    private String[] extractResults(JSONArray array){
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < array.length(); i++){
+            try {
+                JSONObject json = array.getJSONObject(i);
+                String city = parseCity(json);
+                list.add(city);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        String[] cities = new String[list.size()];
+        return list.toArray(cities);
+    }
+
+    private String parseCity(JSONObject json){
+        String city = "";
+        String state = "";
+        String country = "";
+        try {
+            city = json.getString("city");
+            state = json.getString("state");
+            country = json.getString("country_name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return city + " " + state + " " + country;
+
+    }
+}
---
+++ app/src/test/java/com/terminatingcode/android/migrainetree/GeoLookupParserUnitTest.java
@@ -0,0 +1,73 @@
+package com.terminatingcode.android.migrainetree;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class GeoLookupParserUnitTest {
+
+    GeoLookupParser glp;
+
+    @Before
+    public void initialise(){
+        glp = new GeoLookupParser();
+    }
+
+    @Test
+    public void nullJSONObjectReturnsNullMessage() throws JSONException {
+        String[] result = glp.parse(null);
+        String[] expected = {"null JSONObject"};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void lookupReturnsCityStateCountryOneLocation() throws JSONException {
+        String s = "{\"location\": {\"type\":\"CITY\",\"country\":\"US\",\"country_iso3166\":\"US\",\"country_name\":\"USA\",\"state\":\"NY\",\"city\":\"New York\",}}";
+        JSONObject j = new JSONObject(s);
+        String[] result = glp.parse(j);
+        String[] expected = {"New York NY USA"};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void lookupReturnsCityStateCountryMultipleLocations() throws JSONException {
+        String s = "{ \"results\": [{\"name\": \"Paris\",\"city\": \"Paris\",\"state\": \"AR\",\"country\": \"US\",\"country_iso3166\":\"US\",\"country_name\":\"USA\"," +
+                "\"zmw\": \"72855.1.99999\"," +
+                "\"l\": \"/q/zmw:72855.1.99999\"" +
+                "}" +
+                "," +
+                "{" +
+                "\"name\": \"Paris\"," +
+                "\"city\": \"Paris\"," +
+                "\"state\": \"\"," +
+                "\"country\": \"FR\"," +
+                "\"country_iso3166\":\"FR\"," +
+                "\"country_name\":\"France\"," +
+                "\"zmw\": \"00000.37.07156\"," +
+                "\"l\": \"/q/zmw:00000.37.07156\"" +
+                "}" +
+                "]" +
+                "}" +
+                "}";
+        JSONObject j = new JSONObject(s);
+        String[] result = glp.parse(j);
+        String[] expected = {"Paris AR USA", "Paris  France"};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void lookupReturnsQueryNotFound() throws JSONException {
+        String s = "{\"error\": {\"type\": \"querynotfound\",\"description\": \"No cities match your search query\"}}}";
+        JSONObject j = new JSONObject(s);
+        String[] expected = {"city not found"};
+        String[] result = glp.parse(j);
+        assertArrayEquals(expected, result);
+    }
+
+}
